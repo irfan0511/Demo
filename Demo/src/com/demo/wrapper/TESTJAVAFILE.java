79676b9d5a435bb6d8079a593688441bd934654f
irfan <irfan.alam01@infosys.com>
2023-10-04 21:12:20 +0530

    added SOP in TESTJAVAFILE

--- Demo/src/com/demo/wrapper/TESTJAVAFILE.java
+++ Demo/src/com/demo/wrapper/TESTJAVAFILE.java
@@ -3,8 +3,7 @@ package com.demo.wrapper;
 public class TESTJAVAFILE {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Java");
 	}
 
 }
